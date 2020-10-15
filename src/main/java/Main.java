20b6e7747a6333e348cb37356f960d728c10ddde
Robert Zdeb <robinio123@gmail.com>
2020-10-15 20:09:34 +0200

    Prints 3 jokes at one time

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args) {
         ChuckNorrisQuotes jokes = new ChuckNorrisQuotes();
         System.out.println(jokes.getRandomQuote());
+        System.out.println(jokes.getRandomQuote());
+        System.out.println(jokes.getRandomQuote());
     }
 }
